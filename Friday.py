ab9bfae7a05bc62060030456c90ba600ee3cfd90
LoGiCGOD <adutta786610@gmail.com>
2021-04-07 12:38:22 +0530

    various other functionalities added

--- Friday.py
+++ Friday.py
@@ -67,3 +67,60 @@ def takeCommand():
         print("Say that again please...")
         return "None"
     return query
+
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+        query = takeCommand().lower()
+    # User query is converted to lower case
+
+
+# Logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia....')
+
+            #query = query.replace("wikipedia", "")
+
+            # Use Index.summary() function to find the summary of the Index.
+            results = wikipedia.summary(query, sentences=10)
+            speak("According to wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")
+
+        elif 'play music' in query:
+            music_dir = 'C:\\Users\\user\\Desktop\\music'
+            songs = os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir, songs[0]))
+
+        elif 'time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            print(strTime)
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'thank you' in query:
+            speak('Happy to help sir')
+
+        elif "write a note" in query:
+            speak("What should i write, sir")
+            note = takeCommand()
+            file = open('jarvis.txt', 'w')
+            speak("Sir, Should i include date and time")
+            snfm = takeCommand()
+            if 'yes' in snfm or 'sure' in snfm:
+                strTime = datetime.datetime.now().strftime("% H:% M:% S")
+                file.write(strTime)
+                file.write(" :- ")
+                file.write(note)
+            else:
+                file.write(note)
