be97f0a705bdba34af766c6393bef21b392e7782
LoGiCGOD <adutta786610@gmail.com>
2021-04-07 10:01:27 +0530

    imported necessary libraries

---
+++ Friday.py
@@ -0,0 +1,17 @@
+import pyttsx3
+# pyttsx3 is a text to speech conversion library in python
+
+
+import speech_recognition as sr
+import datetime
+# to show currrent date and time
+
+import time
+import wikipedia
+# wiki api used
+
+import webbrowser
+# inbuilt module
+
+import os
+import wolframalpha
