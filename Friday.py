424b3d91b81c438e7d77a2b9e259fbc61509f1c3
LoGiCGOD <adutta786610@gmail.com>
2021-04-07 10:11:16 +0530

    Speak and wish functions generated

--- Friday.py
+++ Friday.py
@@ -15,3 +15,34 @@ import webbrowser
 
 import os
 import wolframalpha
+
+
+engine = pyttsx3.init('sapi5')
+# Microsoft Speech API (SAPI5) is the technology for voice recognition and synthesis provided by Microsoft
+
+
+voices = engine.getProperty('voices')
+# print(voices)
+# print(voices[1].id)
+engine.setProperty('voice', voices[0].id)
+# voices[0] for male & voices[1] for female audio.
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour < 12:
+        speak("Good Morning!, Abhishek")
+        # speak("Abhishek")
+    elif hour >= 12 and hour <= 18:
+        speak("Good Afternoon!, Abhishek")
+        # speak("Abhishek")
+    else:
+        speak("Good Evening!, Abhishek")
+        # speak("Abhishek")
+    newname = ("Logitron online")
+    speak(newname)
